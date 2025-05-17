171f396c331938feebb8cd991519ddd8d3bc7ccb
Totini1234 <farhatantony@gmail.com>
2025-05-17 18:48:44 +0300

    Initial push of final GEMCO site

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-center p-10 text-green-700 font-bold text-3xl">
+      Welcome to the final GEMCO site 🌍💼
+    </div>
+  );
+}
+export default App;
